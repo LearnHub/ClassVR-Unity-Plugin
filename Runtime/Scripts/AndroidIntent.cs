7fa75652d1eeede973a6f89f01d0bc3f009bf28c
Adam Harwood <adam.harwood@avantiseducation.com>
2025-07-03 15:49:03 +0100

    Apply Google style guide

--- Runtime/Scripts/AndroidIntent.cs
+++ Runtime/Scripts/AndroidIntent.cs
@@ -1,87 +1,83 @@
 using System;
 using UnityEngine;
 
-namespace ClassVR
-{
-    /// <summary>
-    /// Utility class for getting data from the Android launch intent
-    /// </summary>
-    public sealed class AndroidIntent
-    {
-        public class ComponentName
-        {
-            public string Class;
-            public string Package;
-        }
-
-        public string Action { get; private set; }
-        public int BroadcastQueueHint { get; private set; }
-        public string[] Categories { get; private set; }
-        public ComponentName Component { get; private set; }
-        public int ContentUserHint { get; private set; }
-        public string Data { get; private set; }
-        public string Extras { get; private set; }
-        public int Flags { get; private set; }
-        public string Package { get; private set; }
-        public string Type { get; private set; }
-
-        private static readonly Lazy<AndroidIntent> lazy = new Lazy<AndroidIntent>(() => new AndroidIntent());
-        public static AndroidIntent Instance { get { return lazy.Value; } }
-
-        private AndroidJavaClass _javaAndroidIntent;
-
-        private AndroidIntent()
-        {
-#if (!UNITY_EDITOR && UNITY_ANDROID)
-            _javaAndroidIntent = new AndroidJavaClass("com.classvr.cvr_unity_java.AndroidIntent");
-
-            var json = _javaAndroidIntent.CallStatic<string>("getIntentData");
-
-            var intent = JsonUtility.FromJson<SerializableIntent>(json);
-
-            Action = intent.mAction;
-            BroadcastQueueHint = intent.mBroadcastQueueHint;
-            Categories = intent.mCategories;
-            Component = new ComponentName
-            {
-                Class = intent.mComponent.mClass,
-                Package = intent.mComponent.mPackage
-            };
-            ContentUserHint = intent.mContentUserHint;
-            Data = intent.mData?.uriString;
-            Extras = intent.mExtras;
-            Flags = intent.mFlags;
-            Package = intent.mPackage;
-            Type = intent.mType;
-#endif
-        }
-
-        [Serializable]
-        class SerializableIntent
-        {
-            [Serializable]
-            public class SerializableComponent
-            {
-                public string mClass;
-                public string mPackage;
-            }
-
-            [Serializable]
-            public class SerializableData
-            {
-                public string uriString;
-            }
-
-            public string mAction;
-            public int mBroadcastQueueHint;
-            public string[] mCategories;
-            public SerializableComponent mComponent;
-            public int mContentUserHint;
-            public SerializableData mData;
-            public string mExtras;  // Extras is an arbitrary JSON object
-            public int mFlags;
-            public string mPackage;
-            public string mType;
-        }
+namespace ClassVR {
+  /// <summary>
+  /// Utility class for getting data from the Android launch intent
+  /// </summary>
+  public sealed class AndroidIntent {
+    public class ComponentName {
+      public string Class;
+      public string Package;
     }
+
+    public string Action { get; private set; }
+    public int BroadcastQueueHint { get; private set; }
+    public string[] Categories { get; private set; }
+    public ComponentName Component { get; private set; }
+    public int ContentUserHint { get; private set; }
+    public string Data { get; private set; }
+    public string Extras { get; private set; }
+    public int Flags { get; private set; }
+    public string Package { get; private set; }
+    public string Type { get; private set; }
+
+    private static readonly Lazy<AndroidIntent> lazy = new Lazy<AndroidIntent>(() => new AndroidIntent());
+    public static AndroidIntent Instance {
+      get {
+        return lazy.Value;
+      }
+    }
+
+    private AndroidJavaClass _javaAndroidIntent;
+
+    private AndroidIntent() {
+#if (!UNITY_EDITOR && UNITY_ANDROID)
+      _javaAndroidIntent = new AndroidJavaClass("com.classvr.cvr_unity_java.AndroidIntent");
+
+      var json = _javaAndroidIntent.CallStatic<string>("getIntentData");
+
+      var intent = JsonUtility.FromJson<SerializableIntent>(json);
+
+      Action = intent.mAction;
+      BroadcastQueueHint = intent.mBroadcastQueueHint;
+      Categories = intent.mCategories;
+      Component = new ComponentName {
+        Class = intent.mComponent.mClass,
+        Package = intent.mComponent.mPackage
+      };
+      ContentUserHint = intent.mContentUserHint;
+      Data = intent.mData?.uriString;
+      Extras = intent.mExtras;
+      Flags = intent.mFlags;
+      Package = intent.mPackage;
+      Type = intent.mType;
+#endif
+    }
+
+    [Serializable]
+    class SerializableIntent {
+      [Serializable]
+      public class SerializableComponent {
+        public string mClass;
+        public string mPackage;
+      }
+
+      [Serializable]
+      public class SerializableData {
+        public string uriString;
+      }
+
+      public string mAction;
+      public int mBroadcastQueueHint;
+      public string[] mCategories;
+      public SerializableComponent mComponent;
+      public int mContentUserHint;
+      public SerializableData mData;
+      public string mExtras;  // Extras is an arbitrary JSON object
+      public int mFlags;
+      public string mPackage;
+      public string mType;
+    }
+  }
 }
--- Runtime/Scripts/CVRProperties.cs
+++ Runtime/Scripts/CVRProperties.cs
@@ -1,159 +1,145 @@
 using System;
 using UnityEngine;
 
-namespace ClassVR
-{
+namespace ClassVR {
+  /// <summary>
+  /// Utility class for getting device info from the ClassVR client
+  /// </summary>
+  public sealed class CVRProperties {
+    public class ConnectInfo {
+      public int Id;
+      public DateTime LastModified;
+    }
+
+    public enum ClientChannel {
+      Unset,
+      Alpha,
+      Beta,
+      Release
+    }
+
+    public string DisplayName { get; private set; }
+    public string Id { get; private set; }
+    public string DeviceSecret { get; private set; }
+    public ClientChannel Channel { get; private set; } = ClientChannel.Unset;
+    public bool TiltToSpin { get; private set; } = true;
+    // Safest defaults are just to use the current time so cached data will be invalidated
+    public DateTime LastModified { get; private set; } = DateTime.Now;
+    public ConnectInfo OrganizationInfo { get; private set; }
+    public string DeviceJWT { get; private set; }
+
+    const int CLASSVR_UNENROLLED_DEVICES = 18579;
+
+    // Singleton pattern with lazy evaluation
+    private static readonly Lazy<CVRProperties> lazy = new Lazy<CVRProperties>(() => new CVRProperties());
+    public static CVRProperties Instance {
+      get { return lazy.Value; }
+    }
+
+    private AndroidJavaClass _javaCVRProperties;
+
     /// <summary>
-    /// Utility class for getting device info from the ClassVR client
+    /// Constructs and queries the ClassVR client to initialize properties.
     /// </summary>
-    public sealed class CVRProperties
-    {
-        public class ConnectInfo
-        {
-            public int Id;
-            public DateTime LastModified;
-        }
-
-        public enum ClientChannel
-        {
-            Unset,
-            Alpha,
-            Beta,
-            Release
-        }
-
-        public string DisplayName { get; private set; }
-        public string Id { get; private set; }
-        public string DeviceSecret { get; private set; }
-        public ClientChannel Channel { get; private set; } = ClientChannel.Unset;
-        public bool TiltToSpin { get; private set; } = true;
-        // Safest defaults are just to use the current time so cached data will be invalidated
-        public DateTime LastModified { get; private set; } = DateTime.Now;
-        public ConnectInfo OrganizationInfo { get; private set; }
-        public string DeviceJWT { get; private set; }
-
-        const int CLASSVR_UNENROLLED_DEVICES = 18579;
-
-        // Singleton pattern with lazy evaluation
-        private static readonly Lazy<CVRProperties> lazy = new Lazy<CVRProperties>(() => new CVRProperties());
-        public static CVRProperties Instance { get { return lazy.Value; } }
-
-        private AndroidJavaClass _javaCVRProperties;
-
-        /// <summary>
-        /// Constructs and queries the ClassVR client to initialize properties.
-        /// </summary>
-        private CVRProperties()
-        {
+    private CVRProperties() {
 #if (!UNITY_EDITOR && UNITY_ANDROID)
-            // Get reference to the Java class used to request serialized properties
-            _javaCVRProperties = new AndroidJavaClass("com.classvr.cvr_unity_java.CVRProperties");
+      // Get reference to the Java class used to request serialized properties
+      _javaCVRProperties = new AndroidJavaClass("com.classvr.cvr_unity_java.CVRProperties");
 #endif
-            Refresh();
-        }
-
-        /// <summary>
-        /// Queries the ClassVR client and updates properties.
-        /// </summary>
-        public void Refresh()
-        {
-            Debug.Log("Refreshing ClassVR client properties");
-
-#if (!UNITY_EDITOR && UNITY_ANDROID)
-            // Request json serialized data from Java plugin
-            var json = _javaCVRProperties.CallStatic<string>("getClassVRProperties");
-            // Deserialize using Unity's JsonUtility
-            var props = JsonUtility.FromJson<SerializableCVRProperties>(json);
-
-            // Copy deserialized values into properties
-
-            Id = props.Id;
-            if (!string.IsNullOrEmpty(Id))
-            {
-                Debug.LogFormat("Found ClassVR device id '{0}'", Id);
-            }
-
-            DeviceSecret = props.DeviceSecret;
-            if (!string.IsNullOrEmpty(DeviceSecret))
-            {
-                Debug.LogFormat("Found ClassVR device secret");
-            }
-
-            DisplayName = props.DisplayName;
-            if (!string.IsNullOrEmpty(DisplayName))
-            {
-                Debug.LogFormat("Found ClassVR device name '{0}'", DisplayName);
-            }
-
-            DeviceJWT = props.DeviceJWT;
-            if (!string.IsNullOrEmpty(DeviceJWT))
-            {
-                Debug.LogFormat("Found ClassVR device JWT '{0}'", DeviceJWT);
-            }
-
-            Channel = props.Channel;
-            Debug.LogFormat("Found ClassVR channel '{0}'", Channel.ToString());
-
-            TiltToSpin = props.TiltToSpin;
-            Debug.LogFormat("Found ClassVR tiltToSpin '{0}'", TiltToSpin.ToString());
-
-            LastModified = props.LastModifiedDate;
-            Debug.LogFormat("Found lastmodified '{0}'", LastModified.ToString());
-
-            // Enrolled organization, will be set to unenrolled if the Java Plugin failed to get data from the ContentProvider
-            OrganizationInfo = props.OrgInfo;
-            Debug.LogFormat("Found organization id '{0}'", OrganizationInfo.Id.ToString());
-            Debug.LogFormat("Found organizationLastModified '{0}'", OrganizationInfo.LastModified.ToString());
-#endif
-        }
+      Refresh();
     }
 
-    // Serializable CVRProperties, used for deserializing from json provided by Java plugin
-    // Accounts for the quirks of Unity's JsonUtility, such as not handling DateTime
+    /// <summary>
+    /// Queries the ClassVR client and updates properties.
+    /// </summary>
+    public void Refresh() {
+      Debug.Log("Refreshing ClassVR client properties");
+
+#if (!UNITY_EDITOR && UNITY_ANDROID)
+      // Request json serialized data from Java plugin
+      var json = _javaCVRProperties.CallStatic<string>("getClassVRProperties");
+      // Deserialize using Unity's JsonUtility
+      var props = JsonUtility.FromJson<SerializableCVRProperties>(json);
+
+      // Copy deserialized values into properties
+
+      Id = props.Id;
+      if (!string.IsNullOrEmpty(Id)) {
+        Debug.LogFormat("Found ClassVR device id '{0}'", Id);
+      }
+
+      DeviceSecret = props.DeviceSecret;
+      if (!string.IsNullOrEmpty(DeviceSecret)) {
+        Debug.LogFormat("Found ClassVR device secret");
+      }
+
+      DisplayName = props.DisplayName;
+      if (!string.IsNullOrEmpty(DisplayName)) {
+        Debug.LogFormat("Found ClassVR device name '{0}'", DisplayName);
+      }
+
+      DeviceJWT = props.DeviceJWT;
+      if (!string.IsNullOrEmpty(DeviceJWT)) {
+        Debug.LogFormat("Found ClassVR device JWT '{0}'", DeviceJWT);
+      }
+
+      Channel = props.Channel;
+      Debug.LogFormat("Found ClassVR channel '{0}'", Channel.ToString());
+
+      TiltToSpin = props.TiltToSpin;
+      Debug.LogFormat("Found ClassVR tiltToSpin '{0}'", TiltToSpin.ToString());
+
+      LastModified = props.LastModifiedDate;
+      Debug.LogFormat("Found lastmodified '{0}'", LastModified.ToString());
+
+      // Enrolled organization, will be set to unenrolled if the Java Plugin failed to get data from the ContentProvider
+      OrganizationInfo = props.OrgInfo;
+      Debug.LogFormat("Found organization id '{0}'", OrganizationInfo.Id.ToString());
+      Debug.LogFormat("Found organizationLastModified '{0}'", OrganizationInfo.LastModified.ToString());
+#endif
+    }
+  }
+
+  // Serializable CVRProperties, used for deserializing from json provided by Java plugin
+  // Accounts for the quirks of Unity's JsonUtility, such as not handling DateTime
+  [Serializable]
+  class SerializableCVRProperties : ISerializationCallbackReceiver {
     [Serializable]
-    class SerializableCVRProperties : ISerializationCallbackReceiver
-    {
-        [Serializable]
-        private class SerializableConnectInfo
-        {
-            public int Id;
-            public string LastModified;
-        }
-
-        public string DisplayName;
-        public string Id;
-        public string DeviceSecret;
-        public CVRProperties.ClientChannel Channel;
-        public bool TiltToSpin;
-        public DateTime LastModifiedDate;
-        public CVRProperties.ConnectInfo OrgInfo;
-        public string DeviceJWT;
-
-        [SerializeField]
-        private string LastModified;
-        [SerializeField]
-        private SerializableConnectInfo OrganizationInfo;
-
-        public void OnAfterDeserialize()
-        {
-            DateTime.TryParse(LastModified, out LastModifiedDate);
-
-            OrgInfo = new CVRProperties.ConnectInfo
-            {
-                Id = OrganizationInfo.Id
-            };
-            DateTime.TryParse(OrganizationInfo.LastModified, out OrgInfo.LastModified);
-        }
-
-        public void OnBeforeSerialize()
-        {
-            LastModified = LastModifiedDate.ToString("O");
-
-            OrganizationInfo = new SerializableConnectInfo
-            {
-                Id = OrgInfo.Id,
-                LastModified = OrgInfo.LastModified.ToString("O")
-            };
-        }
+    private class SerializableConnectInfo {
+      public int Id;
+      public string LastModified;
     }
+
+    public string DisplayName;
+    public string Id;
+    public string DeviceSecret;
+    public CVRProperties.ClientChannel Channel;
+    public bool TiltToSpin;
+    public DateTime LastModifiedDate;
+    public CVRProperties.ConnectInfo OrgInfo;
+    public string DeviceJWT;
+
+    [SerializeField]
+    private string LastModified;
+    [SerializeField]
+    private SerializableConnectInfo OrganizationInfo;
+
+    public void OnAfterDeserialize() {
+      DateTime.TryParse(LastModified, out LastModifiedDate);
+
+      OrgInfo = new CVRProperties.ConnectInfo {
+        Id = OrganizationInfo.Id
+      };
+      DateTime.TryParse(OrganizationInfo.LastModified, out OrgInfo.LastModified);
+    }
+
+    public void OnBeforeSerialize() {
+      LastModified = LastModifiedDate.ToString("O");
+
+      OrganizationInfo = new SerializableConnectInfo {
+        Id = OrgInfo.Id,
+        LastModified = OrgInfo.LastModified.ToString("O")
+      };
+    }
+  }
 }
