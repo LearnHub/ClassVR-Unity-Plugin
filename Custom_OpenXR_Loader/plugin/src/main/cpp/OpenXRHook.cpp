296c2fc367fa124a1400f825b4c493f852bc2f0e
Octavian Vasilovici <tavyhome@gmail.com>
2025-09-17 15:18:01 +0100

    Updated to the Implicit Layer and Tilt-To-Spin extension.

--- Custom_OpenXR_Loader/plugin/src/main/cpp/OpenXRHook.cpp
+++ Custom_OpenXR_Loader/plugin/src/main/cpp/OpenXRHook.cpp
@@ -2,6 +2,7 @@
 #include <dlfcn.h>
 #include <android/log.h>
 #include <string.h>
+#include <vector>
 
 #define LOG_TAG "OpenXRAvanitsLoader"
 #define LOG(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
@@ -9,8 +10,6 @@
 static void* real_loader_handle = nullptr;
 static PFN_xrGetInstanceProcAddr real_xrGetInstanceProcAddr = nullptr;
 static PFN_xrCreateInstance original_xrCreateInstance = nullptr;
-static PFN_xrCreateReferenceSpace original_xrCreateReferenceSpace = nullptr;
-static PFN_xrLocateSpace real_xrLocateSpace = nullptr;
 
 // Load the real OpenXR loader
 static bool load_real_loader() {
@@ -43,73 +42,37 @@ extern "C" XrResult xrCreateInstance(const XrInstanceCreateInfo* createInfo, XrI
         return XR_ERROR_INITIALIZATION_FAILED;
 
     XrInstanceCreateInfo modifiedInfo = *createInfo;
+    std::vector<const char*> requiredExtensionNames(
+            modifiedInfo.enabledExtensionNames,
+            modifiedInfo.enabledExtensionNames + modifiedInfo.enabledExtensionCount
+    );
+    requiredExtensionNames.push_back("XR_EXT_Avantis_tilt_to_spin");
 
-    static const char* layers[] = {
-            "XR_APILAYER_TILTTOSPIN_AVANTIS"
-    };
+    modifiedInfo.enabledExtensionCount = static_cast<uint32_t>(requiredExtensionNames.size());
+    modifiedInfo.enabledExtensionNames = requiredExtensionNames.data();
 
-    modifiedInfo.enabledApiLayerCount = 1;
-    modifiedInfo.enabledApiLayerNames = layers;
+    XrResult res = original_xrCreateInstance(&modifiedInfo, instance);
 
-    LOG("Injecting API layer: %s", layers[0]);
+    if (res != XR_SUCCESS)
+        LOG("xrCreateInstance Failed with %d", res);
+    else
+        LOG("xrCreateInstance Created");
 
-    return original_xrCreateInstance(&modifiedInfo, instance);
-}
 
-// Hooked xrCreateReferenceSpace to log space type
-extern "C" XrResult xrCreateReferenceSpace(XrSession session, const XrReferenceSpaceCreateInfo* createInfo, XrSpace* space) {
-    const char* typeName = "UNKNOWN";
-    switch (createInfo->referenceSpaceType) {
-        case XR_REFERENCE_SPACE_TYPE_VIEW: typeName = "VIEW"; break;
-        case XR_REFERENCE_SPACE_TYPE_LOCAL: typeName = "LOCAL"; break;
-        case XR_REFERENCE_SPACE_TYPE_STAGE: typeName = "STAGE"; break;
-        case XR_REFERENCE_SPACE_TYPE_UNBOUNDED_MSFT: typeName = "UNBOUNDED_MSFT"; break;
-        case XR_REFERENCE_SPACE_TYPE_COMBINED_EYE_VARJO: typeName = "COMBINED_EYE_VARJO"; break;
-        case XR_REFERENCE_SPACE_TYPE_LOCAL_FLOOR_EXT: typeName = "LOCAL_FLOOR_EXT"; break;
+    // Define our function in OpenXR format.
+    typedef XrResult (XRAPI_PTR *PFN_xrEnableTiltToSpinEXT)(XrInstance instance, XrBool32 enable);
+    PFN_xrEnableTiltToSpinEXT xrEnableTiltToSpinEXT = nullptr;
+    xrGetInstanceProcAddr(*instance, "xrEnableTiltToSpinEXT",(PFN_xrVoidFunction *) (&xrEnableTiltToSpinEXT));
+
+    if (xrEnableTiltToSpinEXT) {
+        xrEnableTiltToSpinEXT(*instance, XR_TRUE);
+        LOG("Tilt to Spin Enabled");
     }
-
-    LOG("xrCreateReferenceSpace called with type: %s (%d)", typeName, createInfo->referenceSpaceType);
-
-    if (!original_xrCreateReferenceSpace) {
-        LOG("Original xrCreateReferenceSpace is null!");
-        return XR_ERROR_INITIALIZATION_FAILED;
-    }
-
-    XrResult res = original_xrCreateReferenceSpace(session, createInfo, space);
-    if (res != XR_SUCCESS) {
-        LOG("xrCreateReferenceSpace returned error: %d", res);
+    else
+    {
+        LOG("Tilt to Spin Failed");
     }
     return res;
-}
-
-extern "C" XrResult xrLocateSpace(
-        XrSpace space,
-        XrSpace baseSpace,
-        XrTime time,
-        XrSpaceLocation* location)
-{
-    if (!load_real_loader()) {
-        return XR_ERROR_INITIALIZATION_FAILED;
-    }
-
-    if (!real_xrLocateSpace) {
-        // Resolve original function pointer on first use
-        XrResult res = real_xrGetInstanceProcAddr(XR_NULL_HANDLE, "xrLocateSpace", (PFN_xrVoidFunction*)&real_xrLocateSpace);
-        if (res != XR_SUCCESS) {
-            LOG("Failed to get original xrLocateSpace");
-            return res;
-        }
-    }
-
-    XrResult result = real_xrLocateSpace(space, baseSpace, time, location);
-
-    if (result == XR_ERROR_HANDLE_INVALID)
-    {
-        //LOG("[xrLocateSpace] Suppressed XR_ERROR_HANDLE_INVALID error");
-        return XR_SUCCESS;
-    }
-
-    return result;
 }
 
 // Intercept key function pointers
@@ -123,29 +86,6 @@ extern "C" XrResult xrGetInstanceProcAddr(XrInstance instance, const char* name,
             XrResult res = real_xrGetInstanceProcAddr(instance, name, (PFN_xrVoidFunction*)&original_xrCreateInstance);
             if (res != XR_SUCCESS) {
                 LOG("Failed to get original xrCreateInstance");
-                return res;
-            }
-        }
-        return XR_SUCCESS;
-    }
-
-    if (strcmp(name, "xrCreateReferenceSpace") == 0) {
-        *function = (PFN_xrVoidFunction)xrCreateReferenceSpace;
-        if (!original_xrCreateReferenceSpace) {
-            XrResult res = real_xrGetInstanceProcAddr(instance, name, (PFN_xrVoidFunction*)&original_xrCreateReferenceSpace);
-            if (res != XR_SUCCESS) {
-                LOG("Failed to get original xrCreateReferenceSpace");
-                return res;
-            }
-        }
-        return XR_SUCCESS;
-    }
-    if (strcmp(name, "xrLocateSpace") == 0) {
-        *function = (PFN_xrVoidFunction)xrLocateSpace;
-        if (!real_xrLocateSpace) {
-            XrResult res = real_xrGetInstanceProcAddr(instance, name, (PFN_xrVoidFunction*)&real_xrLocateSpace);
-            if (res != XR_SUCCESS) {
-                LOG("Failed to get original xrLocateSpace");
                 return res;
             }
         }
