9e4c2c110c93bfc18282e759760968f7fe1d3eba
Octavian Vasilovici <tavyhome@gmail.com>
2025-09-17 17:18:16 +0100

    Support for Implicit Layer 2.0.0 & 2.1.0 for tilt-to-spin.

--- Custom_OpenXR_Loader/plugin/src/main/cpp/OpenXRHook.cpp
+++ Custom_OpenXR_Loader/plugin/src/main/cpp/OpenXRHook.cpp
@@ -10,6 +10,7 @@
 static void* real_loader_handle = nullptr;
 static PFN_xrGetInstanceProcAddr real_xrGetInstanceProcAddr = nullptr;
 static PFN_xrCreateInstance original_xrCreateInstance = nullptr;
+static PFN_xrEnumerateInstanceExtensionProperties real_xrEnumerateInstanceExtensionProperties = nullptr;
 
 // Load the real OpenXR loader
 static bool load_real_loader() {
@@ -33,6 +34,7 @@ static bool load_real_loader() {
     LOG("Loaded real OpenXR loader and xrGetInstanceProcAddr: %p", real_xrGetInstanceProcAddr);
     return true;
 }
+//--------------------------------------------------------------------------------------------------
 
 // Hooked xrCreateInstance to inject API layer
 extern "C" XrResult xrCreateInstance(const XrInstanceCreateInfo* createInfo, XrInstance* instance) {
@@ -46,7 +48,40 @@ extern "C" XrResult xrCreateInstance(const XrInstanceCreateInfo* createInfo, XrI
             modifiedInfo.enabledExtensionNames,
             modifiedInfo.enabledExtensionNames + modifiedInfo.enabledExtensionCount
     );
-    requiredExtensionNames.push_back("XR_EXT_Avantis_tilt_to_spin");
+
+    bool isAvantisSupported_v1 = false;
+    bool isAvantisSupported_v2 = false;
+    const char* TILT_TO_SPIN_V1 = "XR_EXT_Avantis_tilt_to_spin";
+    const char* TILT_TO_SPIN_V2 = "XR_AVN_tilt_to_spin";
+    uint32_t extCount = 0;
+
+    // get count
+    if (xrEnumerateInstanceExtensionProperties(nullptr, 0, &extCount, nullptr) == XR_SUCCESS) {
+        std::vector<XrExtensionProperties> extProps(extCount, {XR_TYPE_EXTENSION_PROPERTIES});
+
+        // Second call: get properties
+        if (xrEnumerateInstanceExtensionProperties(nullptr, extCount, &extCount, extProps.data()) == XR_SUCCESS)
+        {
+            for (const auto& ext : extProps)
+            {
+                if (strcmp(ext.extensionName, TILT_TO_SPIN_V1) == 0)
+                {
+                    isAvantisSupported_v1 = true;
+                }
+                else if (strcmp(ext.extensionName, TILT_TO_SPIN_V2) == 0)
+                {
+                    isAvantisSupported_v2 = true;
+                }
+            }
+        }
+    }
+
+    // Push it once after the enum finishes
+    if (isAvantisSupported_v1)
+        requiredExtensionNames.push_back(TILT_TO_SPIN_V1);
+    else if (isAvantisSupported_v2)
+        requiredExtensionNames.push_back(TILT_TO_SPIN_V2);
+
 
     modifiedInfo.enabledExtensionCount = static_cast<uint32_t>(requiredExtensionNames.size());
     modifiedInfo.enabledExtensionNames = requiredExtensionNames.data();
@@ -60,20 +95,36 @@ extern "C" XrResult xrCreateInstance(const XrInstanceCreateInfo* createInfo, XrI
 
 
     // Define our function in OpenXR format.
-    typedef XrResult (XRAPI_PTR *PFN_xrEnableTiltToSpinEXT)(XrInstance instance, XrBool32 enable);
-    PFN_xrEnableTiltToSpinEXT xrEnableTiltToSpinEXT = nullptr;
-    xrGetInstanceProcAddr(*instance, "xrEnableTiltToSpinEXT",(PFN_xrVoidFunction *) (&xrEnableTiltToSpinEXT));
-
-    if (xrEnableTiltToSpinEXT) {
-        xrEnableTiltToSpinEXT(*instance, XR_TRUE);
-        LOG("Tilt to Spin Enabled");
-    }
-    else
+    if (isAvantisSupported_v1)
     {
-        LOG("Tilt to Spin Failed");
+        typedef XrResult (XRAPI_PTR *PFN_xrEnableTiltToSpinEXT)(XrInstance instance, XrBool32 enable);
+        PFN_xrEnableTiltToSpinEXT xrEnableTiltToSpinEXT = nullptr;
+        xrGetInstanceProcAddr(*instance, "xrEnableTiltToSpinEXT",(PFN_xrVoidFunction *) (&xrEnableTiltToSpinEXT));
+
+        if (xrEnableTiltToSpinEXT){
+            xrEnableTiltToSpinEXT(*instance, XR_TRUE);
+            LOG("Tilt to Spin Enabled");
+        } else {
+            LOG("Tilt to Spin Failed");
+        }
+    }
+    if (isAvantisSupported_v2)
+    {
+        typedef XrResult (XRAPI_PTR *PFN_xrEnableTiltToSpinAVN)(XrInstance instance, XrBool32 enable);
+        PFN_xrEnableTiltToSpinAVN xrEnableTiltToSpinAVN = nullptr;
+        xrGetInstanceProcAddr(*instance, "xrEnableTiltToSpinAVN",(PFN_xrVoidFunction *) (&xrEnableTiltToSpinAVN));
+
+        if (xrEnableTiltToSpinAVN)
+        {
+            xrEnableTiltToSpinAVN(*instance, XR_TRUE);
+            LOG("Tilt to Spin Enabled");
+        } else {
+            LOG("Tilt to Spin Failed");
+        }
     }
     return res;
 }
+//--------------------------------------------------------------------------------------------------
 
 // Intercept key function pointers
 extern "C" XrResult xrGetInstanceProcAddr(XrInstance instance, const char* name, PFN_xrVoidFunction* function) {
@@ -93,6 +144,44 @@ extern "C" XrResult xrGetInstanceProcAddr(XrInstance instance, const char* name,
     }
     return real_xrGetInstanceProcAddr(instance, name, function);
 }
+//--------------------------------------------------------------------------------------------------
+
+extern "C" XrResult XRAPI_PTR xrEnumerateInstanceExtensionProperties(const char* layerName, uint32_t propertyCapacityInput, uint32_t* propertyCountOutput, XrExtensionProperties* properties)
+{
+    if (!load_real_loader())
+        return XR_ERROR_INITIALIZATION_FAILED;
+
+    // Resolve the real function once
+    if (!real_xrEnumerateInstanceExtensionProperties)
+    {
+        real_xrEnumerateInstanceExtensionProperties = (PFN_xrEnumerateInstanceExtensionProperties)dlsym(real_loader_handle,"xrEnumerateInstanceExtensionProperties");
+        if (!real_xrEnumerateInstanceExtensionProperties)
+        {
+            LOG("Failed to find real xrEnumerateInstanceExtensionProperties");
+            return XR_ERROR_FUNCTION_UNSUPPORTED;
+        }
+    }
+
+    XrResult res = real_xrEnumerateInstanceExtensionProperties(
+            layerName,
+            propertyCapacityInput,
+            propertyCountOutput,
+            properties
+    );
+
+
+    if (XR_SUCCEEDED(res) && layerName == nullptr && properties && *propertyCountOutput < propertyCapacityInput)
+    {
+        XrExtensionProperties customExt{XR_TYPE_EXTENSION_PROPERTIES};
+        strncpy(customExt.extensionName, "XR_EXT_Avantis_tilt_to_spin", XR_MAX_EXTENSION_NAME_SIZE);
+        customExt.extensionVersion = 1;
+        properties[(*propertyCountOutput)++] = customExt;
+        LOG("Injected XR_EXT_Avantis_tilt_to_spin into extension list");
+    }
+
+    return res;
+}
+//--------------------------------------------------------------------------------------------------
 
 // Forward xrEnumerateApiLayerProperties
 extern "C" XrResult xrEnumerateApiLayerProperties(
@@ -116,3 +205,4 @@ extern "C" XrResult xrEnumerateApiLayerProperties(
 
     return func(propertyCapacityInput, propertyCountOutput, properties);
 }
+//--------------------------------------------------------------------------------------------------
