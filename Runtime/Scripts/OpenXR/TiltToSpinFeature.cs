e5b4b30e87a55686d585ee606377638abc137e77
Adam Harwood <adam.harwood@avantiseducation.com>
2026-02-05 15:39:10 +0000

    Add tilt to spin OpenXR feature
    Squashed commit of the following:
    
    commit 99c2852703efa5c1846636f0bcb68eea45a416c3
    Author: Adam Harwood <adam.harwood@avantiseducation.com>
    Date:   Thu Feb 5 15:35:28 2026 +0000
    
        Add tilt to spin updates to CHANGELOG and README
    
    commit 89a83d6d098cceebed930860a5573a1b1fce2744
    Author: Adam Harwood <adam.harwood@avantiseducation.com>
    Date:   Thu Feb 5 14:22:06 2026 +0000
    
        Enable tilt to spin by default when feature enabled
    
    commit 50fb8ca9c0d2fd8970f1f2d5c0a836e70664d5b1
    Author: Adam Harwood <adam.harwood@avantiseducation.com>
    Date:   Thu Feb 5 12:22:37 2026 +0000
    
        Move TiltToSpinFeature to OpenXR dir and add namespace
    
    commit f0386bcac27458e6bcc88c85bf4c79aa1f56f202
    Author: Adam Harwood <adam.harwood@avantiseducation.com>
    Date:   Thu Feb 5 12:09:34 2026 +0000
    
        Add Unity.XR.OpenXR dependency to asmdef and manifest
    
        Required for the tilt to spin feature
    
    commit 8a2f3e71b691acd54a3a9eb355eb73bd3a0fd91d
    Author: Adam Harwood <adam.harwood@avantiseducation.com>
    Date:   Thu Feb 5 12:08:46 2026 +0000
    
        WIP implementation of tilt to spin feature

---
+++ Runtime/Scripts/OpenXR/TiltToSpinFeature.cs
@@ -0,0 +1,198 @@
+using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.XR.OpenXR;
+using UnityEngine.XR.OpenXR.Features;
+using UnityEngine.XR.OpenXR.NativeTypes;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.XR.OpenXR.Features;
+#endif
+
+namespace ClassVR.OpenXR {
+#if UNITY_EDITOR
+  [OpenXRFeature(
+      UiName = "ClassVR Tilt-To-Spin",
+      BuildTargetGroups = new[] { BuildTargetGroup.Android },
+      Company = "Avantis Education",
+      Desc = "Enables XR_AVN_tilt_to_spin",
+      OpenxrExtensionStrings = "XR_AVN_tilt_to_spin",
+      Version = "1.0.0",
+      FeatureId = "com.avantis.openxr.tilttospin"
+  )]
+#endif
+  /// <summary>
+  /// Enables the Avantis Tilt-to-Spin extension for OpenXR.
+  /// This feature converts head roll (tilt) into yaw rotation (spin) for VR navigation.
+  /// It is not recommended to use this class directly, use XR Plug-in Management in Project Settings instead.
+  ///
+  /// Usage Example:
+  /// <code>
+  /// // Get the feature instance
+  /// var tiltToSpinFeature = OpenXRSettings.Instance.GetFeature&lt;ClassVrTiltToSpinFeature&gt;();
+  ///
+  /// if (tiltToSpinFeature != null) {
+  ///   // Enable tilt-to-spin functionality
+  ///   if (tiltToSpinFeature.EnableTiltToSpin(true)) {
+  ///     Debug.Log("Tilt-to-spin enabled successfully!");
+  ///   }
+  /// }
+  /// </code>
+  /// </summary>
+  public class ClassVrTiltToSpinFeature : OpenXRFeature {
+    // The extension string constant
+    public const string ExtensionName = "XR_AVN_tilt_to_spin";
+
+    // XrBool32 constants
+    private const uint XR_TRUE = 1;
+    private const uint XR_FALSE = 0;
+
+    // Instance state
+    private ulong xrInstance = 0;
+
+    // Function delegates (cached after OnInstanceCreate)
+    private xrEnableTiltToSpinAVNDelegate xrEnableTiltToSpinAVN;
+    private xrEnableResetOnRecenterTiltToSpinAVNDelegate xrEnableResetOnRecenterTiltToSpinAVN;
+
+    #region Native Delegates
+    /// <summary>
+    /// Delegate for xrGetInstanceProcAddr function.
+    /// </summary>
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    private delegate XrResult GetInstanceProcAddrDelegate(
+        ulong instance,
+        string name,
+        ref IntPtr function);
+
+    /// <summary>
+    /// Delegate for xrEnableTiltToSpinAVN function.
+    /// </summary>
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    private delegate XrResult xrEnableTiltToSpinAVNDelegate(
+        ulong instance,
+        uint enable);
+
+    /// <summary>
+    /// Delegate for xrEnableResetOnRecenterTiltToSpinAVN function.
+    /// </summary>
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    private delegate XrResult xrEnableResetOnRecenterTiltToSpinAVNDelegate(
+        ulong instance,
+        uint isResetEnabled);
+    #endregion
+
+    #region Lifecycle Methods
+    /// <summary>
+    /// Called when the OpenXR instance is created.
+    /// Retrieves function pointers for the extension functions.
+    /// </summary>
+    protected override bool OnInstanceCreate(ulong xrInstanceHandle) {
+      // Check the extension is enabled
+      if (!OpenXRRuntime.IsExtensionEnabled(ExtensionName)) {
+        Debug.LogError($"[AvantisTiltToSpin] {ExtensionName} extension is not enabled.");
+        return false;
+      }
+
+      xrInstance = xrInstanceHandle;
+
+      // Get xrGetInstanceProcAddr function
+      var xrGetInstanceProcAddrPtr = xrGetInstanceProcAddr;
+      if (xrGetInstanceProcAddrPtr == IntPtr.Zero) {
+        Debug.LogError("[AvantisTiltToSpin] Failed to get xrGetInstanceProcAddr");
+        return false;
+      }
+
+      var getInstanceProcAddr = Marshal.GetDelegateForFunctionPointer<GetInstanceProcAddrDelegate>(xrGetInstanceProcAddrPtr);
+
+      // Retrieve function pointers
+      var funcPtr = IntPtr.Zero;
+      XrResult result;
+
+      // Get xrEnableTiltToSpinAVN
+      result = getInstanceProcAddr(xrInstance, "xrEnableTiltToSpinAVN", ref funcPtr);
+      if (result.IsSuccess() && funcPtr != IntPtr.Zero) {
+        xrEnableTiltToSpinAVN = Marshal.GetDelegateForFunctionPointer<xrEnableTiltToSpinAVNDelegate>(funcPtr);
+      } else {
+        Debug.LogError("[AvantisTiltToSpin] Failed to get xrEnableTiltToSpinAVN function pointer");
+        return false;
+      }
+
+      // Get xrEnableResetOnRecenterTiltToSpinAVN
+      funcPtr = IntPtr.Zero;
+      result = getInstanceProcAddr(xrInstance, "xrEnableResetOnRecenterTiltToSpinAVN", ref funcPtr);
+      if (result.IsSuccess() && funcPtr != IntPtr.Zero) {
+        xrEnableResetOnRecenterTiltToSpinAVN = Marshal.GetDelegateForFunctionPointer<xrEnableResetOnRecenterTiltToSpinAVNDelegate>(funcPtr);
+      } else {
+        Debug.LogWarning("[AvantisTiltToSpin] Failed to get xrEnableResetOnRecenterTiltToSpinAVN function pointer");
+      }
+
+      // Enable tilt to spin by default
+      EnableTiltToSpin(true);
+
+      return xrEnableTiltToSpinAVN != null;
+    }
+
+    /// <summary>
+    /// Called when the OpenXR instance is destroyed.
+    /// Clears cached function pointers and state.
+    /// </summary>
+    protected override void OnInstanceDestroy(ulong xrInstanceHandle) {
+      xrInstance = 0;
+      xrEnableTiltToSpinAVN = null;
+      xrEnableResetOnRecenterTiltToSpinAVN = null;
+    }
+    #endregion
+
+    #region Public API
+    /// <summary>
+    /// Enable or disable the tilt-to-spin functionality.
+    /// When enabled, head roll is converted to yaw rotation.
+    /// Enabled by default.
+    /// </summary>
+    /// <param name="enable">True to enable tilt-to-spin, false to disable.</param>
+    /// <returns>True if the operation succeeded, false otherwise.</returns>
+    public bool EnableTiltToSpin(bool enable) {
+      if (xrEnableTiltToSpinAVN == null) {
+        Debug.LogError("[AvantisTiltToSpin] Cannot call EnableTiltToSpin - function not loaded. Ensure the feature is enabled in OpenXR settings.");
+        return false;
+      }
+
+      uint xrBoolValue = enable ? XR_TRUE : XR_FALSE;
+      XrResult result = xrEnableTiltToSpinAVN(xrInstance, xrBoolValue);
+
+      if (!result.IsSuccess()) {
+        Debug.LogError($"[AvantisTiltToSpin] xrEnableTiltToSpinAVN failed with result: {result}");
+        return false;
+      }
+
+      Debug.Log($"[AvantisTiltToSpin] Tilt-to-spin {(enable ? "enabled" : "disabled")}");
+      return true;
+    }
+
+    /// <summary>
+    /// Enable or disable automatic reset of tilt-to-spin state when tracking recenters.
+    /// When enabled, the accumulated yaw rotation is reset to identity when a recenter event occurs.
+    /// </summary>
+    /// <param name="enable">True to enable reset on recenter, false to disable.</param>
+    /// <returns>True if the operation succeeded, false otherwise.</returns>
+    public bool EnableResetOnRecenter(bool enable) {
+      if (xrEnableResetOnRecenterTiltToSpinAVN == null) {
+        Debug.LogError("[AvantisTiltToSpin] Cannot call EnableResetOnRecenter - function not loaded.");
+        return false;
+      }
+
+      uint xrBoolValue = enable ? XR_TRUE : XR_FALSE;
+      XrResult result = xrEnableResetOnRecenterTiltToSpinAVN(xrInstance, xrBoolValue);
+
+      if (!result.IsSuccess()) {
+        Debug.LogError($"[AvantisTiltToSpin] xrEnableResetOnRecenterTiltToSpinAVN failed with result: {result}");
+        return false;
+      }
+
+      Debug.Log($"[AvantisTiltToSpin] Reset on recenter {(enable ? "enabled" : "disabled")}");
+      return true;
+    }
+    #endregion
+  }
+}
